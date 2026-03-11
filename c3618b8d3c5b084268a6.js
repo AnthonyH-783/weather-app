02104093cb6a4ade9915043bb85d82f21e92a13d
Anthony <anthonnhani@gmail.com>
2026-03-11 16:42:13 -0600

    Deployment commit

---
+++ c3618b8d3c5b084268a6.js
@@ -0,0 +1,56 @@
+import {WeatherAPICaller} from "./APICaller.js";
+import * as Renderer from "./renderer.js";
+import { FormHandler } from "./formHandler.js";
+import { ErrorHandler } from "./errorHandler.js";
+import "./styles.css";
+
+
+window.onload = () => {
+    const today_container = document.querySelector(".current-container");
+    const today_forecast = today_container.querySelector(".todays-forecast");
+    const hourly_grid = today_container.querySelector(".hourly-grid");
+    const cond_grid = document.querySelector(".cond-grid");
+    const weekly_forecast = document.querySelector(".weekly-forecast");
+    const today = new WeatherAPICaller("Edmonton", "today");
+    const week = new WeatherAPICaller("Edmonton", "next7days");
+    today.call().then((today_data) => {
+            week.call().then((week_data) => {
+            const dom_params = {today_forecast, weekly_forecast, hourly_grid, cond_grid};
+            Renderer.renderPage(dom_params, {today_data, week_data});
+        });
+    });
+}
+(function ScreenController() {
+    // Retrieving main dom elements
+    const form = document.querySelector("form");
+    const today_container = document.querySelector(".current-container");
+    const today_forecast = today_container.querySelector(".todays-forecast");
+    const hourly_grid = today_container.querySelector(".hourly-grid");
+    const cond_grid = document.querySelector(".cond-grid");
+    const weekly_forecast = document.querySelector(".weekly-forecast");
+    const error_dom = form.querySelector(".error");
+    const error_handler = new ErrorHandler(error_dom);
+
+    addSubmissionListener();
+
+    function addSubmissionListener(){
+            form.addEventListener("submit", async (evt) => {
+                evt.preventDefault();
+                const form_handler = new FormHandler(form);
+                const location = form_handler.input.value;
+                try{
+                    const data = await form_handler.getFullForecast(location);
+                    const dom = {form, today_container, today_forecast, hourly_grid, cond_grid, weekly_forecast};
+                    Renderer.renderPage(dom, data);
+                }
+                catch(error){
+                    error_handler.displayError();
+                    console.error(error);
+                }
+                
+
+
+
+    });}
+
+})();
